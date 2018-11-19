56c76b0fe465e5f737cdc4428547c5eff5b16a06
yumemi-r-sato <r_sato@yumemi.co.jp>
2018-11-19 16:54:51 +0900

    fix

---
+++ index.js
@@ -0,0 +1,14 @@
+javascript: (function() {
+    const iframe = document.querySelector('iframe');
+    const body = iframe.contentWindow.document.body;
+
+    // 10時に出社して19時に帰る
+    body.querySelector('select[name="time_in_hour"]').value = 10;
+    body.querySelector('select[name="time_out_hour"]').value = 19;
+
+    // 休憩1時間
+    body.querySelector('select[name="time_break_input_hour"]').value = 1;
+
+    // 稼働8時間
+    body.querySelector('select[name="time_required_hour1"]').value = 8;
+})();
