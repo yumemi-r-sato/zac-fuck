e2e4f67255238a766711b183a6cd57d75a62f1b6
yumemi-r-sato <r_sato@yumemi.co.jp>
2018-11-20 16:35:42 +0900

    fix:スーパーzac-fuck爆誕

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # zac-fuck
 
 ## Description
+
+### src/normal.js
+
 - 出社時刻
   - 10時0分
 - 退社時刻
@@ -11,11 +14,25 @@
 - 稼働時間
   - 1番上のやつが8時間
 
+### src/getOutTime.js
+
+- 出社時刻
+  - 10時0分
+- 退社時刻
+  - *現在時刻から取得*
+- 休憩時間
+  - 1時間0分
+
+- 稼働時間
+  - 1番上のやつが8時間
+
 ## Usage
+
 1. copy build.js.
 2. create bookmark, paste inside url.
 3. in ZAC, click this bookmarklet.
 
 ## develop
-1. edit index.js.
-2. Go to [/packer/](http://dean.edwards.name/packer/)
+
+1. edit jsfile
+2. `yarn build`
--- src/normal.js
+++ src/normal.js
@@ -2,15 +2,22 @@ javascript: (function() {
     const iframe = document.querySelector('iframe');
     const body = iframe.contentWindow.document.body;
 
-    // 10時に出社して19時に帰る
+    // 出社時刻
     body.querySelector('select[name="time_in_hour"]').value = 10;
+    // o or 15 or 30 or 45
+    body.querySelector('select[name="time_in_minute"]').value = 0;
+
+    // 退社時刻
     body.querySelector('select[name="time_out_hour"]').value = 19;
+    // o or 15 or 30 or 45
+    body.querySelector('select[name="time_out_minute"]').value = 0;
 
     // 休憩1時間
     body.querySelector('select[name="time_break_input_hour"]').value = 1;
 
     // 稼働8時間
     body.querySelector('select[name="time_required_hour1"]').value = 8;
+    body.querySelector('select[name="time_required_minute1"]').value = 0;
 
     alert('✨書き換え完了✨');
 })();
