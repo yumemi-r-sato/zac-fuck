735c5a6bf537d47f29e22ef3567f157d37f267f1
yumemi-r-sato <r_sato@yumemi.co.jp>
2018-11-22 13:10:44 +0900

    fix:テンプレートを書き換え

--- src/normal.js
+++ src/normal.js
@@ -3,9 +3,9 @@ javascript: (function() {
     const body = iframe.contentWindow.document.body;
 
     // 出社時刻
-    body.querySelector('select[name="time_in_hour"]').value = 10;
+    body.querySelector('select[name="time_in_hour"]').value = 11;
     // o or 15 or 30 or 45
-    body.querySelector('select[name="time_in_minute"]').value = 0;
+    body.querySelector('select[name="time_in_minute"]').value = 30;
 
     // 退社時刻
     body.querySelector('select[name="time_out_hour"]').value = 19;
@@ -16,8 +16,8 @@ javascript: (function() {
     body.querySelector('select[name="time_break_input_hour"]').value = 1;
 
     // 稼働8時間
-    body.querySelector('select[name="time_required_hour1"]').value = 8;
-    body.querySelector('select[name="time_required_minute1"]').value = 0;
+    body.querySelector('select[name="time_required_hour1"]').value = 6;
+    body.querySelector('select[name="time_required_minute1"]').value = 30;
 
     alert('✨書き換え完了✨');
 })();
